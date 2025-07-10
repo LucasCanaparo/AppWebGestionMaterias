7d89056995b0e0044c10b7bfe949711d985bb409
Lucas <lucascanaparo@gmail.com>
2025-07-09 23:27:05 -0300

    cambios menores

--- frontend/app/components/Menu.jsx
+++ frontend/app/components/Menu.jsx
@@ -8,7 +8,7 @@ export default function Menu() {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous"></link>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
           <div className="container text-center p-4 bg-dark rounded shadow-sm" style={{ maxWidth: '700px' }}>
-            <h1 className="fw-bold mb-3 text-light">Malla Interactiva de Ingenierías</h1>
+            <h1 className="fw-bold mb-3 text-light">Malla Interactiva de Materias</h1>
             <h5 className="mb-4 text-secondary">Seleccione su Universidad:</h5>
     
             <div className="row g-3 justify-content-center">
