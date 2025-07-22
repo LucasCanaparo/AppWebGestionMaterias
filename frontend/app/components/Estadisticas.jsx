37ba137693d4deba9d14f000b4c671f01d2904f1
GOAT Luquitas <402620@industrial.frc.utn.edu.ar>
2025-07-22 12:40:27 -0300

    Tabla estadistica

--- frontend/app/components/Estadisticas.jsx
+++ frontend/app/components/Estadisticas.jsx
@@ -1,10 +1,69 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default function Estadisticas() {
+
+    const [materias, setMaterias] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/materiasMecanica`)
+            .then(res => setMaterias(res.data))
+            .catch(err => console.error("Error al cargar materias:", err));
+    }, []);
+
+    const aprobadas = materias.filter(m => m.aprobada);
+    const noAprobadas = materias.filter(m => !m.aprobada);
+
     return (
-        <div>
-            <h1>hOLA, SOY ESTADISITICAS</h1>
+        <div className="container mt-4 text-light">
+
+            <h2>Estadisticas de tu carrera</h2>
+
+            <div className="row mt-4">
+                <div className="col-md-6">
+                    <h4>Aprobadas</h4>
+                    <table className="table table-dark table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Materia</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {aprobadas.map((m, i) => (
+                                <tr key={m.id}>
+                                    <td>{i + 1}</td>
+                                    <td>{m.nombre}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="col-md-6">
+                    <h4>Pendientes</h4>
+                    <table className="table table-dark table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Materia</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {noAprobadas.map((m, i) => (
+                                <tr key={m.id}>
+                                    <td>{i + 1}</td>
+                                    <td>{m.nombre}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+
+
             <Link to='/'>
                 <button className='btn btn-secondary'
                     style={{
--- frontend/app/components/Navbar.jsx
+++ frontend/app/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">MiApp</a>
+                    <a class="navbar-brand" href="#"><em>Malla Interactiva</em></a>
 
                     <button
                         class="navbar-toggler"
