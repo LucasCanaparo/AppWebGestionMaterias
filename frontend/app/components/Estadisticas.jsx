c6b6adc84b0f0b72ee4385a1c70757c28a0b1af6
GOAT Luquitas <402620@industrial.frc.utn.edu.ar>
2025-07-22 12:22:46 -0300

    agregando btn de volver a menu

--- frontend/app/components/Estadisticas.jsx
+++ frontend/app/components/Estadisticas.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Estadisticas() {
-  return (
-    <div>
-      <h1>hOLA, SOY ESTADISITICAS</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>hOLA, SOY ESTADISITICAS</h1>
+            <Link to='/'>
+                <button className='btn btn-secondary'
+                    style={{
+                        position: 'absolute',
+                        top: '20px',
+                        left: '20px',
+                        zIndex: 1000
+                    }}>Volver al menú</button>
+            </Link>
+        </div>
+    )
 }
