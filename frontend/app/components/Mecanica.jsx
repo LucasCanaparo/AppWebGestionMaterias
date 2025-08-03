512de1ef8e5a1d49b17e8fd1cce177fd4d4fe53d
Lucas <lucascanaparo@gmail.com>
2025-08-03 01:22:46 -0300

    cambios ligeros

--- frontend/app/components/Mecanica.jsx
+++ frontend/app/components/Mecanica.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-//import '../CSS/Mecanica.css'
 
 export default function Mecanica() {
 
